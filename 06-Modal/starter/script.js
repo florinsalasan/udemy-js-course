9b2e975e6bf7595f0f5af958514633d7d29ede6a
Florin Salasan <florinsalasan@gmail.com>
2022-09-28 13:10:13 -0400

    added the js logic to show and hide the modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+let modal = document.querySelector('.modal')
+let overlay = document.querySelector('.overlay')
+let closeBtn = document.querySelector('.close-modal')
+let elements = [closeBtn, overlay]
+
+document.querySelectorAll('.show-modal').forEach(modalBtn => {
+  modalBtn.addEventListener('click', function revealModal() {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+
+  })
+})
+
+function hideModal() {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+elements.forEach(element =>
+  element.addEventListener('click', function () {
+    hideModal()
+  }
+  )
+)
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key)
+  if (e.key === 'Escape') {
+    hideModal()
+  }
+})
