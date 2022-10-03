7b54455ed6afaa0ee5f21885b7aaa97b5974dad4
Florin Salasan <florinsalasan@gmail.com>
2022-10-03 12:41:49 -0400

    finished challenge 2

--- 09-Data-Structures-Operators/starter/coding-challenges.js
+++ 09-Data-Structures-Operators/starter/coding-challenges.js
@@ -83,3 +83,65 @@ printGoals('Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels')
 console.log((game.odds.team1 > game.odds.team2 && game.team2) || game.team1)
 
 printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich')
+
+
+
+// CHALLENGE 2
+
+// Let's continue with our football betting app! Keep using the 'game' variable from before.
+// Your tasks:
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33 Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the same property names 😉
+// 4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+//      {
+//        Gnarby: 1,
+//        Hummels: 1,
+//        Lewandowski: 2
+// }
+
+console.log('CHALLENGE 2')
+
+for (let i = 0; i < game.scored.length; i++) {
+  console.log(`Goal ${i + 1}: ${game.scored[i]}`)
+}
+
+let odd_total = 0
+for (const odd in game.odds) {
+  odd_total = odd_total + game.odds[odd]
+
+}
+
+console.log(odd_total)
+// console.log(Object.keys(game.odds).)
+console.log(`${(odd_total / Object.keys(game.odds).length).toFixed(2)}`)
+
+
+for (const odd in game.odds) {
+  if (odd !== 'x') {
+    let actual_name = game[odd]
+    console.log(`Odd of victory ${actual_name}: ${game.odds[odd]}`)
+  }
+  else {
+    console.log(`Odd of draw: ${game.odds[odd]}`)
+  }
+}
+
+let scorers = {
+
+}
+
+for (const scorer of game.scored) {
+  let keys = Object.keys(scorers)
+  if (keys.includes(scorer)) {
+    scorers[scorer] += 1
+  }
+  else {
+    scorers[scorer] = 1
+  }
+}
+
+console.log(scorers)
