102cf27e751ce048c8ebe04dacd9d685d3e916d3
Florin Salasan <florinsalasan@gmail.com>
2022-10-05 12:01:45 -0400

    finished challenge 1

---
+++ 10-Functions/starter/coding-challenge.js
@@ -0,0 +1,76 @@
+// CHALLENGE 1
+
+// Let's build a simple poll app!
+// A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter 'poll' object below.
+// Your tasks:
+// 1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+// 1.1. Display a prompt window for the user to input the number of the
+// selected option. The prompt should look like this: What is your favourite programming language?
+// 0: JavaScript
+// 1: Python
+// 2: Rust
+// 3: C++
+// (Write option number)
+// 1.2. Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
+// 2. Call this method whenever the user clicks the "Answerpoll" button.
+// 3. Create a method 'displayResults' which displays the poll results. The
+// method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+// 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+// 5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword look like in this situation?
+
+// Test data for bonus:
+// § Data1:[5,2,3]
+// § Data2:[1,5,3,9,6,1]
+
+
+
+const poll = {
+  question: "What is your favourite programming language?", options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section! 
+  answers: new Array(4).fill(0),
+};
+
+console.log('CHALLENGE 1')
+
+poll.registerNewAnswer = function () {
+  console.log(this)
+  let response = prompt(`${poll.question} select from the following: \n${poll.options.join('\r\n')} \n (Write in the option number please)`)
+
+  let optionValues = [...Array(4).keys()]
+  console.log(optionValues.includes(Number(response)))
+
+  let response2 = -1
+  if (!optionValues.includes(Number(response))) {
+    while (!optionValues.includes(Number(response2))) {
+      response2 = prompt(`Your answer was not one of the listed options, please select an option from the list below. \n\n${poll.question} select from the following: \n${poll.options.join('\r\n')} \n (Write in the option number please)`)
+    }
+  }
+
+  response2 !== -1 ? addValue(response2) : addValue(response)
+
+  console.log(poll.answers)
+  this.displayResults('string')
+  this.displayResults('other')
+}
+
+// generalize with this keyword, otherwise cannot use call method to display answers that are not in poll object
+poll.displayResults = function (type) {
+  if (type === 'string') {
+    console.log(`Poll results are ${this.answers.join(', ')}.`)
+  } else {
+    console.log(this.answers)
+  }
+}
+
+function addValue(value) {
+  poll.answers[value]++
+}
+
+// bind the this keyword to poll so that the event listener will target the poll object when the user interacts with it as intended.
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
+
+// call the method with set answers
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [5, 2, 3] }, 'other');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,160 @@
 'use strict';
+
+const flight = 'LM234'
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 23984321024
+}
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LM999'
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23984321024) {
+    console.log('checked in')
+  }
+  else {
+    console.log('wrong passport')
+  }
+}
+
+checkIn(flight, jonas)
+
+// console.log(flight)
+// console.log(jonas)
+
+//
+// const flightNum = flight
+// const passenger = jonas
+
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000)
+}
+
+newPassport(jonas)
+checkIn(flight, jonas)
+
+// note js does not have pass by reference, only pass by value weird behaviour with objects though as those pass in their memory reference as a value ????
+
+const oneWord = function (string) {
+  return string.replace(/ /g, '').toLowerCase()
+}
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ')
+}
+
+// higher-order function 
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`)
+  console.log(`Transformed string: ${fn(str)}`)
+
+  console.log(`Transformed by: ${fn.name}`)
+}
+
+transformer('JavaScript is the best!', upperFirstWord)
+transformer('JavaScript is the best!', oneWord)
+
+//using higher order functions allows for abstraction
+
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`)
+  }
+}
+
+const greeterHey = greet('Hey')
+greeterHey('Flo')
+greeterHey('Steve')
+
+greet('Hello')('Flo')
+
+const greeter = greeting => name => console.log(`${greeting} ${name}`)
+
+greeter('Hola')('Flo')
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LM',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
+
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name })
+  }
+}
+
+lufthansa.book(239, 'Flo Sal')
+lufthansa.book(635, 'Joe Schmo')
+console.log(lufthansa)
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+
+}
+
+const book = lufthansa.book
+
+// the call method allows us to set the 'this' keyword manually to the first argument
+book.call(eurowings, 23, 'Sarah Williams')
+console.log(eurowings)
+
+book.call(lufthansa, 23, 'Sarah Williams')
+console.log(lufthansa)
+
+// this will work with any airline object that has the same properties and functions
+
+const flightData = [583, 'George Cooper']
+
+book.apply(eurowings, flightData)
+book.call(eurowings, ...flightData)
+
+// binding creates a new function that makes the this keyword apply to whatever it is bound to.
+const bookEw = book.bind(eurowings)
+
+bookEw(23, 'Steve')
+
+// can also path in arguments so that they can be preset, called partial application, where some arguments are predefined in a function
+const bookEw23 = book.bind(eurowings, 23)
+
+bookEw23('Bob')
+
+// binding with event listeners
+
+lufthansa.planes = 300
+lufthansa.buyPlane = function () {
+  console.log(this)
+
+  this.planes++
+  console.log(this.planes)
+}
+
+// need to bind to lufthansa, otherwise, the 'this' keyword points to the button with the '.buy' class which would not allow the function to work as intended.
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa))
+
+// partial application
+const addTax = (rate, value) => value + value * rate
+
+console.log(addTax(0.1, 200))
+
+const addVAT = addTax.bind(null, 0.23)
+
+// addVAT = (value) => value + value * 0.23
+console.log(addTax(0.23, 200))
+console.log(addVAT(200))
+console.log(addVAT(15))
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate
+  }
+}
+
+const addVAT2 = addTaxRate(0.23)
+console.log(addVAT2(200))
+console.log(addVAT2(15))
