4e2dd339fd2175bd2cd646f3be242d8c2db4f04f
Florin Salasan <florinsalasan@gmail.com>
2022-11-02 11:58:20 -0400

    finished up challenge 3, inheritance between constructor functions

--- 14-OOP/starter/coding-challenge.js
+++ 14-OOP/starter/coding-challenge.js
@@ -137,3 +137,51 @@ const ford = new CarCl('Ford', 120);
 // ford.brake();
 // ford.brake();
 // ford.brake();
+
+// CHALLENGE 3
+
+// 1. Use a constructor function to implement an ElectricCar (called'EV') as a child "class" of 'Car'. Besides a make and current speed, the 'EV' also has the current battery charge in % ('charge' property)
+// 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo'
+// 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%'
+// 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! Hint: Review the definiton of polymorphism 😉
+// Test data:
+// § Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+const ElectricCar = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+ElectricCar.prototype = Object.create(Car.prototype);
+ElectricCar.prototype.constructor = ElectricCar;
+
+ElectricCar.prototype.printSpeed = function () {
+  console.log(
+    `This ${this.make} is travelling at ${this.speed} kph and has a remaining charge of ${this.charge}%`
+  );
+};
+
+ElectricCar.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+ElectricCar.prototype.accelerate = function () {
+  this.charge -= 1;
+  if (this.charge > 0) {
+    this.speed += 20;
+    this.printSpeed();
+  } else {
+    console.log(`The ${this.make} has ran out of battery!`);
+  }
+};
+
+const tesla = new ElectricCar('Tesla', 25, 5);
+
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.chargeBattery(15);
+// tesla.printSpeed();
+// tesla.brake();
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,198 +1,232 @@
 'use strict';
 
-// cannot use an arrow function for a constructor function since it does not have a 'this' keyword attached
-const Person = function (firstName, birthYear) {
-  console.log(this);
-  // logged: Person {}
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// // cannot use an arrow function for a constructor function since it does not have a 'this' keyword attached
+// // const Person = function (firstName, birthYear) {
+// //   console.log(this);
+// //   // logged: Person {}
+// //   this.firstName = firstName;
+// //   this.birthYear = birthYear;
 
-  // Do not make functions inside of a constructor, bad performance since each instance of an object made with the constructor will contain the method.
-  // this.calcAge = function() {
-  //   console.log(2037 - this.birthYear)
-  // }
-};
+// //   // Do not make functions inside of a constructor, bad performance since each instance of an object made with the constructor will contain the method.
+// //   // this.calcAge = function() {
+// //   //   console.log(2037 - this.birthYear)
+// //   // }
+// // };
 
-const flo = new Person('Florin', 1999);
-console.log(flo);
-// logs the type of Object and the firstname and birthyear values
+// // const flo = new Person('Florin', 1999);
+// // console.log(flo);
+// // // logs the type of Object and the firstname and birthyear values
 
-// 1. New object is created, {}
-// 2. constructor function is called, this = {} the object above in 1.
-// 3. {} linked to the prototype
-// 4. function automatically returns {}
+// // // 1. New object is created, {}
+// // // 2. constructor function is called, this = {} the object above in 1.
+// // // 3. {} linked to the prototype
+// // // 4. function automatically returns {}
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1989);
+// // const matilda = new Person('Matilda', 2017);
+// // const jack = new Person('Jack', 1989);
 
-console.log(matilda, jack);
+// // console.log(matilda, jack);
 
-// Constructor functions kind of act as a class in js since classes do not exist, thus you can colloquially call the new people made with a constructor instances of the constructor
+// // Constructor functions kind of act as a class in js since classes do not exist, thus you can colloquially call the new people made with a constructor instances of the constructor
 
-///////////////////////////////////
-//
-//          Prototypes
-//
-///////////////////////////////////
-console.log(Person.prototype);
+// ///////////////////////////////////
+// //
+// //          Prototypes
+// //
+// ///////////////////////////////////
+// // console.log(Person.prototype);
 
-Person.prototype.calcAge = function () {
-  console.log(2077 - this.birthYear);
-};
+// // Person.prototype.calcAge = function () {
+// //   console.log(2077 - this.birthYear);
+// // };
 
-// the calcAge is not a method directly in 'flo' but inherits the method through prototypal inheritance and there is only one method created in the prototype.
-flo.calcAge();
+// // // the calcAge is not a method directly in 'flo' but inherits the method through prototypal inheritance and there is only one method created in the prototype.
+// // flo.calcAge();
 
-console.log(flo.__proto__);
-console.log(flo.__proto__ === Person.prototype); // Logs true
+// // console.log(flo.__proto__);
+// // console.log(flo.__proto__ === Person.prototype); // Logs true
 
-console.log(Person.prototype.isPrototypeOf(flo)); // also true
-console.log(Person.prototype.isPrototypeOf(Person)); // logs false
+// // console.log(Person.prototype.isPrototypeOf(flo)); // also true
+// // console.log(Person.prototype.isPrototypeOf(Person)); // logs false
 
-// Can set
-Person.prototype.species = 'Homo Sapiens';
-console.log(flo);
-console.log(flo.species);
-console.log(flo.hasOwnProperty('firstName')); //true
-console.log(flo.hasOwnProperty('species')); //false since it's not directly on the flo object, instead it is inherited.
+// // // Can set
+// // Person.prototype.species = 'Homo Sapiens';
+// // console.log(flo);
+// // console.log(flo.species);
+// // console.log(flo.hasOwnProperty('firstName')); //true
+// // console.log(flo.hasOwnProperty('species')); //false since it's not directly on the flo object, instead it is inherited.
 
-// prototype chain goes null <- Object.prototype <- Person.prototype <- A Person object
-console.log(flo.__proto__); // Person.prototype
-console.log(flo.__proto__.__proto__); // Object.prototype, shows all of the methods that are available to all Objects
-console.log(flo.__proto__.__proto__.__proto__); // null
+// // // prototype chain goes null <- Object.prototype <- Person.prototype <- A Person object
+// // console.log(flo.__proto__); // Person.prototype
+// // console.log(flo.__proto__.__proto__); // Object.prototype, shows all of the methods that are available to all Objects
+// // console.log(flo.__proto__.__proto__.__proto__); // null
 
-console.dir(Person.prototype.constructor);
-const arr = [3, 8, 4, 10, 10, 3, 4]; // new Array === []
-console.log(arr.__proto__ === Array.prototype); // true
+// // console.dir(Person.prototype.constructor);
+// // const arr = [3, 8, 4, 10, 10, 3, 4]; // new Array === []
+// // console.log(arr.__proto__ === Array.prototype); // true
 
-// can add new method to Arrays, typically not a good idea to extend the prototype to built-in types/objects
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
+// // // can add new method to Arrays, typically not a good idea to extend the prototype to built-in types/objects
+// // Array.prototype.unique = function () {
+// //   return [...new Set(this)];
+// // };
 
-console.log(arr.unique());
+// // console.log(arr.unique());
 
-const h1 = document.querySelector('h1');
-// everything is a huge prototype chain
-console.dir(h1);
-console.dir(x => x + 1);
+// // const h1 = document.querySelector('h1');
+// // // everything is a huge prototype chain
+// // console.dir(h1);
+// // console.dir(x => x + 1);
 
-// ES6 classes
-// classes count as a special function, this is just a nice syntax nothing really changes under the hood, still deals with the prototype and chains.
+// // ES6 classes
+// // classes count as a special function, this is just a nice syntax nothing really changes under the hood, still deals with the prototype and chains.
 
-//// class expression
-// const PersonCl = class {};
+// //// class expression
+// // const PersonCl = class {};
 
-//// class declaration
-class PersonCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+// //// class declaration
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
-  // methods get added to the prototype property
-  // instance methods
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  }
+//   // methods get added to the prototype property
+//   // instance methods
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
 
-  get age() {
-    return 2037 - this.birthYear;
-  }
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
 
-  // when setting a property that already exists avoid using the same variable name
-  set fullName(name) {
-    console.log(name);
-    if (name.includes(' ')) this._fullName = name;
-    // to avoid a conflict with the constructor property
-    else alert(`${name} is not a full name!`);
-  }
+//   // when setting a property that already exists avoid using the same variable name
+//   set fullName(name) {
+//     console.log(name);
+//     if (name.includes(' ')) this._fullName = name;
+//     // to avoid a conflict with the constructor property
+//     else alert(`${name} is not a full name!`);
+//   }
 
-  get fullName() {
-    return this._fullName;
-  }
+//   get fullName() {
+//     return this._fullName;
+//   }
 
-  // static method by adding the static keyword
-  static hey() {
-    console.log(`Hey there 👋`);
-    console.log(this);
-  }
-}
+//   // static method by adding the static keyword
+//   static hey() {
+//     console.log(`Hey there 👋`);
+//     console.log(this);
+//   }
+// }
 
-const hope = new PersonCl('Hope Davis', 1996);
-console.log(hope);
-hope.calcAge();
-console.log(hope.__proto__ === PersonCl.prototype); // true
+// const hope = new PersonCl('Hope Davis', 1996);
+// console.log(hope);
+// hope.calcAge();
+// console.log(hope.__proto__ === PersonCl.prototype); // true
 
-// Can still add methods to the class prototype if you want
-PersonCl.prototype.greet = function () {
-  console.log(`Hey ${this.firstName}`);
-};
-hope.greet();
-console.log(hope.age);
+// // Can still add methods to the class prototype if you want
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+// hope.greet();
+// console.log(hope.age);
 
-// Some notes: make sure to understand the under the hood material before using classes to avoid unexpected issues
-// 1. Classes are not hoisted
-// 2. Classes are first-class citizens
-// 3. Classes are executed in strict mode even if not enabled on the file
+// // Some notes: make sure to understand the under the hood material before using classes to avoid unexpected issues
+// // 1. Classes are not hoisted
+// // 2. Classes are first-class citizens
+// // 3. Classes are executed in strict mode even if not enabled on the file
 
-// Setters and getters
-const account = {
-  owner: 'Flo',
-  movements: [200, 530, 120, 300],
+// // Setters and getters
+// const account = {
+//   owner: 'Flo',
+//   movements: [200, 530, 120, 300],
 
-  get latest() {
-    return this.movements.slice(-1).pop();
-  },
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
 
-  set latest(mov) {
-    this.movements.push(mov);
-  },
-};
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
 
-console.log(account.latest); // 300
-account.latest = 50;
-console.log(account.movements); // [200, 530, 120, 300, 50]
-console.log(account.latest); // 50
+// console.log(account.latest); // 300
+// account.latest = 50;
+// console.log(account.movements); // [200, 530, 120, 300, 50]
+// console.log(account.latest); // 50
 
-// now to add them to the PersonCl class // They're in there now
-// messing around with static methods
-// ie Number.parseFloat() is a static method that is in the Number namespace, it can be called from the as shown above, but it cannot be called directly on a number. It is attached to the constructor but not available on a number directly
+// // now to add them to the PersonCl class // They're in there now
+// // messing around with static methods
+// // ie Number.parseFloat() is a static method that is in the Number namespace, it can be called from the as shown above, but it cannot be called directly on a number. It is attached to the constructor but not available on a number directly
 
-// Creating a static method
+// // Creating a static method
 
-Person.hey = function (person) {
-  console.log(`Hey there ${person.firstName} 👋`);
-  console.log(this); // this === the person constructor
-};
+// Person.hey = function (person) {
+//   console.log(`Hey there ${person.firstName} 👋`);
+//   console.log(this); // this === the person constructor
+// };
 
-// Person.hey(); // would work if there was no arguments to pass in.
-// flo.hey(); // this does not work, says it is not a function
-Person.hey(flo); // this works
+// // Person.hey(); // would work if there was no arguments to pass in.
+// // flo.hey(); // this does not work, says it is not a function
+// Person.hey(flo); // this works
 
-PersonCl.hey(); // works
-// hope.hey(); // once again, not a function
+// PersonCl.hey(); // works
+// // hope.hey(); // once again, not a function
 
-const PersonProto = {
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  },
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
 
-  init(name, birthYear) {
-    this.name = name;
-    this.birthYear = birthYear;
-  },
-};
+//   init(name, birthYear) {
+//     this.name = name;
+//     this.birthYear = birthYear;
+//   },
+// };
 
-const steven = Object.create(PersonProto);
-console.log(steven);
-steven.name = 'Steven';
-steven.birthYear = 2002;
+// const steven = Object.create(PersonProto);
+// console.log(steven);
+// steven.name = 'Steven';
+// steven.birthYear = 2002;
 
-steven.calcAge();
+// steven.calcAge();
 
-const sarah = Object.create(PersonProto);
-sarah.init('Sarah', 1979);
-sarah.calcAge();
+// const sarah = Object.create(PersonProto);
+// sarah.init('Sarah', 1979);
+// sarah.calcAge();
 
 // Object.create is important in being able to make true class inheritance
+
+// Implementing real inheritance between classes, not limited to just prototype inheritance. Will inherit between constructor functions.
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// need to use object.create to set the proper prototype chain, if you try to skip the Object.create call it would point to the same prototype object rather than linking them
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'CS');
+console.log(mike.__proto__); // this returns a Person object with the introduce method and with the prototype that includes the calcAge method
+// Mike should be labelled as a Student rather than a Person can 'hack' around this, maybe this is the best way to fix it, by simply setting the prototype constructor back to Student
+
+// These are all true because of the prototype chaining
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
+
+Student.prototype.constructor = Student;
